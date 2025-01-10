0242b7bc2596d24e2096c3a3b04df9b12b405353
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-01-10 21:30:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// Click Close to close
+const Macbutton1 = document.getElementById("Macbutton1");
+const behind = document.getElementById("behind");
+const Main = document.getElementById("closeMain");
+
+Macbutton1.onclick = function () {
+  Main.style.display = "none";
+};
+
+behind.onclick = function () {
+  Main.style.display = "block";
+};
+
+
+// Minimize
+const Macbutton2 = document.getElementById("Macbutton2");
+
+Macbutton2.onclick = function () {
+  document.body.classList.toggle("minimizeCalc");
+};
+
+
+// Maximize
+const Macbutton3 = document.getElementById("Macbutton3");
+
+Macbutton3.onclick = function () {
+  document.body.classList.toggle("maximizeCalc");
+};
+
+
+// Calculator Code
+const display = document.getElementById("display");
+
+function DisplayKeys(input) {
+  display.value += input;
+}
+
+function del() {
+  display.value = display.value.slice(0, -1);
+}
+
+function Clear() {
+  display.value = "";
+}
+
+function Calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = " Error";
+  }
+}
